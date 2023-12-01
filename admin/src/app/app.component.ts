6f3fe9a7ed2de6a097c5549ef0b8456dd0bb8b9e
Felipe Batista <fbatista@mail.austral.edu.ar>
2023-12-01 17:04:49 -0300

    feat: app architecture

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HomeComponent } from '../components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- admin/src/app/app.routes.ts
+++ admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'admin', component: HomeComponent, title: 'Panel de control | Inicio' },
+    { path: '', component: LoginComponent, title: 'Panel de control | Registro'}
+];
