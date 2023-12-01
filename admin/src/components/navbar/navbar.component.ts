e255abc209f37850b1d0dc25c84f0a4659d01f53
Felipe Batista <fbatista@mail.austral.edu.ar>
2023-12-01 17:03:54 -0300

    feat: navbar component

---
+++ admin/src/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isLoginPage: boolean = false;
+
+  constructor() {
+    if (window.location.pathname === '/') {
+      this.isLoginPage = true;
+    }
+  }
+}
