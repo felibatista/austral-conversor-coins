fe6cd322d8b687cecc92d567bab60a5b0416ff02
Felipe Batista <hellojavaa@gmail.com>
2023-12-10 23:14:39 -0300

    feat: currency finder in list

--- public/src/components/conversor-currencies/conversor-currencies.component.ts
+++ public/src/components/conversor-currencies/conversor-currencies.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Currency } from '../../lib/types';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-conversor-currencies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './conversor-currencies.component.html',
   styleUrl: './conversor-currencies.component.css'
 })
-export class ConversorCurrenciesComponent {
+export class ConversorCurrenciesComponent implements OnInit {
   @Input() currencies: Currency[] = [];
   @Input() actual: Currency | null = null;
   @Input() type: string = '';
@@ -17,9 +18,14 @@ export class ConversorCurrenciesComponent {
   
   @Output() resultChange: EventEmitter<number> = new EventEmitter<number>();
   @Output() actualChange: EventEmitter<Currency> = new EventEmitter<Currency>();
+
+  currenciesCopy: Currency[] = [];
+  find = '';
   
   updateCurrency(currency: Currency): void {
     this.actual = currency;
+    this.find = '';
+    this.currenciesCopy = this.currencies;
     this.actualChange.emit(currency);
     this.resultChange.emit(0);
 
@@ -27,4 +33,20 @@ export class ConversorCurrenciesComponent {
   }
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.currenciesCopy = this.currencies;
+  }
+
+  changeFind(): void {
+    this.currenciesCopy = this.currencies.filter((currency) => {
+      return currency.code.toLowerCase().includes(this.find.toLowerCase());
+    });
+  }
+
+  close(){
+    this.find = '';
+    this.currenciesCopy = this.currencies;
+    window.document.getElementById(this.type + '-dropdown-currency')?.blur();
+  }
 }
